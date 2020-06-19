eb6a46a6e535c4b445520a994ae7c6ebae28b764
senthil15-12 <67093765+senthil15-12@users.noreply.github.com>
2020-06-19 12:49:40 +0530

    positive numbers
    prints positive numbers

---
+++ positive numbers.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#break
+a=10
+while a>10:
+    if(a!=5)
+    print()
+
+
+# In[ ]:
+
+
+filename=str(input("Enter a filename with extension"))
+import numpy as n
+array1=n.array( filename )
+leng=len(filename)
+while (leng>0):
+    leng=leng-1
+    if(leng!="."):
+        print(array1)
+    else:
+            break
+print(array1)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
